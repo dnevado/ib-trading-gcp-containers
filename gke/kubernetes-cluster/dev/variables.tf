fe5ab1964c517161f6f53edc4c30165dfcead62d
AzureAD\DavidNevado <David.Nevado@Kyndryl.com>
2022-11-20 20:44:28 +0100

    added env variables

---
+++ gke/kubernetes-cluster/dev/variables.tf
@@ -0,0 +1,19 @@
+variable "project_id" {
+    type = string
+    default = "api-project-786272790820" 
+}
+variable "region" {
+    type = string
+    default = "europe-southwest1" 
+
+}
+variable "zone" {
+    type = string
+    default = "europe-southwest1-a" 
+
+}
+// ["dev", "prod"]
+variable "env" {
+    type    = string
+    default = "dev"
+}
--- gke/kubernetes-cluster/prod/backend.tf
+++ gke/kubernetes-cluster/prod/backend.tf
@@ -16,6 +16,6 @@
 terraform {
   backend "gcs" {
     bucket = "api-project-786272790820-tfstate"
-    prefix = "envs/prod"
+    prefix = "envs/${var.env}"
   }
 }
