995c7acc6454b4c7616ded21f6c5e4a084fadbbe
AzureAD\DavidNevado <David.Nevado@Kyndryl.com>
2022-12-04 19:33:30 +0100

    Temporary auth for cloud build network

--- gke/kubernetes-cluster/dev/main.tf
+++ gke/kubernetes-cluster/dev/main.tf
@@ -12,12 +12,12 @@ provider "google" {
   zone    = var.zone
 }
 
-resource "google_compute_network" "ib_trading_net" {
-  provider = google-beta
-  project = var.project_id
-  name = "ib-trading-net-${var.env}"
-  auto_create_subnetworks = false
-}
+# resource "google_compute_network" "ib_trading_net" {
+#   provider = google-beta
+#   project = var.project_id
+#   name = "ib-trading-net-${var.env}"
+#   auto_create_subnetworks = false
+# }
 
 #tfimport-terraform import google_compute_subnetwork.ib_trading_subnet __project__/europe-southwest1/ib-trading-subnet
 resource "google_compute_subnetwork" "ib_trading_subnet" {
@@ -188,4 +188,198 @@ resource "google_artifact_registry_repository" "ib-gateway" {
   repository_id = "ib-gateway-${var.env}"
   description   = "ib-gateway ${var.env}"
   format        = "DOCKER" 
+}
+
+
+/* START NETWORKING CONTENT */
+
+resource "google_compute_ha_vpn_gateway" "ha_gateway1" {
+  region  = var.region
+  name    = "ha-vpn-1"
+  network = google_compute_network.ib_trading_net.id
+}
+
+resource "google_compute_ha_vpn_gateway" "ha_gateway2" {
+  region  = var.region
+  name    = "ha-vpn-2"
+  network = google_compute_network.network2.id
+}
+
+# resource "google_compute_network" "ib_trading_net" {
+#   provider = google-beta
+#   project = var.project_id
+#   name = "ib-trading-net-${var.env}"
+#   auto_create_subnetworks = false
+# }
+
+
+resource "google_compute_network" "ib_trading_net" {
+  
+  name = "ib-trading-net-${var.env}"
+  # name                    = "network1"
+  routing_mode            = "GLOBAL"
+  project = var.project_id
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_network" "network2" {
+  name                    = "network2"
+  routing_mode            = "GLOBAL"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "network1_subnet1" {
+  name          = "ha-vpn-subnet-1"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = var.region
+  network       = google_compute_network.ib_trading_net.id
+}
+
+resource "google_compute_subnetwork" "network1_subnet2" {
+  name          = "ha-vpn-subnet-2"
+  ip_cidr_range = "10.0.2.0/24"
+  region        = var.region
+  network       = google_compute_network.ib_trading_net.id
+}
+
+resource "google_compute_subnetwork" "network2_subnet1" {
+  name          = "ha-vpn-subnet-3"
+  ip_cidr_range = "192.168.1.0/24"
+  region        = var.region
+  network       = google_compute_network.network2.id
+}
+
+resource "google_compute_subnetwork" "network2_subnet2" {
+  name          = "ha-vpn-subnet-4"
+  ip_cidr_range = "192.168.2.0/24"
+  region        = "us-east1"
+  network       = google_compute_network.network2.id
+}
+
+resource "google_compute_router" "router1" {
+  name    = "ha-vpn-router1"
+  network = google_compute_network.ib_trading_net.name
+  bgp {
+    asn = 64514
+  }
+}
+
+resource "google_compute_router" "router2" {
+  name    = "ha-vpn-router2"
+  network = google_compute_network.network2.name
+  bgp {
+    asn = 64515
+  }
+}
+
+resource "google_compute_vpn_tunnel" "tunnel1" {
+  name                  = "ha-vpn-tunnel1"
+  region                = var.region
+  vpn_gateway           = google_compute_ha_vpn_gateway.ha_gateway1.id
+  peer_gcp_gateway      = google_compute_ha_vpn_gateway.ha_gateway2.id
+  shared_secret         = "a secret message"
+  router                = google_compute_router.router1.id
+  vpn_gateway_interface = 0
+}
+
+resource "google_compute_vpn_tunnel" "tunnel2" {
+  name                  = "ha-vpn-tunnel2"
+  region                = var.region
+  vpn_gateway           = google_compute_ha_vpn_gateway.ha_gateway1.id
+  peer_gcp_gateway      = google_compute_ha_vpn_gateway.ha_gateway2.id
+  shared_secret         = "a secret message"
+  router                = google_compute_router.router1.id
+  vpn_gateway_interface = 1
+}
+
+resource "google_compute_vpn_tunnel" "tunnel3" {
+  name                  = "ha-vpn-tunnel3"
+  region                = var.region
+  vpn_gateway           = google_compute_ha_vpn_gateway.ha_gateway2.id
+  peer_gcp_gateway      = google_compute_ha_vpn_gateway.ha_gateway1.id
+  shared_secret         = "a secret message"
+  router                = google_compute_router.router2.id
+  vpn_gateway_interface = 0
+}
+
+resource "google_compute_vpn_tunnel" "tunnel4" {
+  name                  = "ha-vpn-tunnel4"
+  region                = var.region
+  vpn_gateway           = google_compute_ha_vpn_gateway.ha_gateway2.id
+  peer_gcp_gateway      = google_compute_ha_vpn_gateway.ha_gateway1.id
+  shared_secret         = "a secret message"
+  router                = google_compute_router.router2.id
+  vpn_gateway_interface = 1
+}
+
+resource "google_compute_router_interface" "router1_interface1" {
+  name       = "router1-interface1"
+  router     = google_compute_router.router1.name
+  region     = var.region
+  ip_range   = "169.254.0.1/30"
+  vpn_tunnel = google_compute_vpn_tunnel.tunnel1.name
+}
+
+resource "google_compute_router_peer" "router1_peer1" {
+  name                      = "router1-peer1"
+  router                    = google_compute_router.router1.name
+  region                    = var.region
+  peer_ip_address           = "169.254.0.2"
+  peer_asn                  = 64515
+  advertised_route_priority = 100
+  interface                 = google_compute_router_interface.router1_interface1.name
+}
+
+resource "google_compute_router_interface" "router1_interface2" {
+  name       = "router1-interface2"
+  router     = google_compute_router.router1.name
+  region     = var.region
+  ip_range   = "169.254.1.2/30"
+  vpn_tunnel = google_compute_vpn_tunnel.tunnel2.name
+}
+
+resource "google_compute_router_peer" "router1_peer2" {
+  name                      = "router1-peer2"
+  router                    = google_compute_router.router1.name
+  region                    = var.region
+  peer_ip_address           = "169.254.1.1"
+  peer_asn                  = 64515
+  advertised_route_priority = 100
+  interface                 = google_compute_router_interface.router1_interface2.name
+}
+
+resource "google_compute_router_interface" "router2_interface1" {
+  name       = "router2-interface1"
+  router     = google_compute_router.router2.name
+  region     = var.region
+  ip_range   = "169.254.0.2/30"
+  vpn_tunnel = google_compute_vpn_tunnel.tunnel3.name
+}
+
+resource "google_compute_router_peer" "router2_peer1" {
+  name                      = "router2-peer1"
+  router                    = google_compute_router.router2.name
+  region                    = var.region
+  peer_ip_address           = "169.254.0.1"
+  peer_asn                  = 64514
+  advertised_route_priority = 100
+  interface                 = google_compute_router_interface.router2_interface1.name
+}
+
+resource "google_compute_router_interface" "router2_interface2" {
+  name       = "router2-interface2"
+  router     = google_compute_router.router2.name
+  region     = var.region
+  ip_range   = "169.254.1.1/30"
+  vpn_tunnel = google_compute_vpn_tunnel.tunnel4.name
+}
+
+resource "google_compute_router_peer" "router2_peer2" {
+  name                      = "router2-peer2"
+  router                    = google_compute_router.router2.name
+  region                    = var.region
+  peer_ip_address           = "169.254.1.2"
+  peer_asn                  = 64514
+  advertised_route_priority = 100
+  interface                 = google_compute_router_interface.router2_interface2.name
 }
