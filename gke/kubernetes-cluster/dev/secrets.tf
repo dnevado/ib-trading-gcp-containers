40192b6970d1a0184bd7c07048993283459a98c1
AzureAD\DavidNevado <David.Nevado@Kyndryl.com>
2023-02-18 11:19:54 +0100

    Fixing env variables user password for TWS

--- gke/kubernetes-cluster/dev/secrets.tf
+++ gke/kubernetes-cluster/dev/secrets.tf
@@ -46,7 +46,7 @@ resource "google_secret_manager_secret_version" "credentials-ibpassword-gw-paper
   provider = google-beta
 
   secret      = google_secret_manager_secret.credentials-ibpassword-gw-paper.id
-  secret_data = "Initial password  10203040 ex2"
+  secret_data = "nevadodM1!"
 }
 
 
